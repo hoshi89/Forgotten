e62585bf59b5d513d8b318749641c3f6638ac804
Jimmy <nintendojimmy@hotmail.com>
2013-02-06 16:28:05 +0100

    Updated Inventory and gui
    Able to load and draw images

--- Forgotten/Forgotten/Gui.cpp
+++ Forgotten/Forgotten/Gui.cpp
@@ -1,6 +1,6 @@
 #include "Gui.h"
 
-Gui::Gui(sf::Vector2f position) : m_position(position){
+Gui::Gui(sf::Vector2f position) : m_position(position), down(false){
 	LoadImage();
 	m_inventorySprite.setPosition(m_position);
 	m_inventorySprite.scale(0.75f, 0.75f);
@@ -24,13 +24,9 @@ void Gui::Draw(sf::RenderWindow &window){
 
 void Gui::Render(sf::RenderWindow &window){
 	const float SPEED = 1.2f;
-	bool down = false;
 	if(m_inventorySprite.getPosition().y < 0 && !down){
 		Move(SPEED);
-	}
-	else if(down){
-		Move(-SPEED);
-	}else{
+	}else if(m_inventorySprite.getPosition().y > -10 && !down){
 		down = true;
 	}
 	Draw(window);
