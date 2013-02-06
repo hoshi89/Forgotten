901759e5c8b8b84d21fb6528f63d66d108f0c146
Robin Engström <engstrom.robin@gmail.com>
2013-02-06 11:11:22 +0100

    LevelManager again

--- Forgotten/Forgotten/LevelManager.h
+++ Forgotten/Forgotten/LevelManager.h
@@ -1,10 +1,14 @@
 #ifndef INCLUDED_LEVELMANAGER
 #define INCLUDED_LEVELMANAGER
 
+#include "Level.h"
+#include <vector>
+
 class LevelManager{
 public:
 	void LoadLevel(/* LEVEL1.dat */);
 private:
+	std::vector<Level*> m_levelRooms;
 };
 
 #endif
