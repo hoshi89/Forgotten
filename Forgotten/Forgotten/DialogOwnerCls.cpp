4a3f68210579e183406bb89a7e118ec4b8df09a6
Robin Engström <engstrom.robin@gmail.com>
2013-02-08 10:18:48 +0100

    Fix dialogowner

--- Forgotten/Forgotten/DialogOwnerCls.cpp
+++ Forgotten/Forgotten/DialogOwnerCls.cpp
@@ -5,35 +5,6 @@ DialogOwnerCls::DialogOwnerCls(const char *aDeckName, int aXpos, int aYpos) : mD
 {
 }
 
-<<<<<<< HEAD
-void DialogOwnerCls::BeginDialog()
-{
-	//den här funktionen anropas när dialogen ska börja..
-}
-
-void DialogOwnerCls::AddToInventory()
-{
-	//default är det ett objekt som lägger till sig själv till inventory
-	//override för NPC lägger till objekt som NPC:n ger dig
-	//InventoryVector.pushback(objekt) ..
-	//Den här funktionen skickar ALLTID det som inventory ska innehålla!
-}
-
-void DialogOwnerCls::PlaceText(DialogCls aDialog)
-{
-}
-
-
-sf::IntRect DialogOwnerCls::getQuestionRect()
-{
-	if(mDialog.PlayerOwnCard())
-	{
-		m_player->GetPosition();
-	}
-	return sf::IntRect();
-
-}
-=======
 //void DialogOwnerCls::BeginDialog()
 //{
 //	//den här funktionen anropas när dialogen ska börja..
@@ -60,7 +31,7 @@ sf::IntRect DialogOwnerCls::getQuestionRect()
 //	}
 //
 //}
->>>>>>> 90f646568dcd44ab91c7c44b6a14f58e5f2d37f2
+
 DialogOwnerCls::~DialogOwnerCls(void)
 {
 }
